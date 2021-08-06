32f08e7b7d8b0d9aac37dd8060616c8560b7def4
steve ng <steevineer@gmail.com>
2021-08-06 17:55:28 -0500

    mod init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/stnguyenn/go-cli
+
+go 1.16
+
+require github.com/sirupsen/logrus v1.8.1
--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"github.com/sirupsen/logrus"
+)
+
 func main() {
-  
+	greeting := "world"
+	logrus.Debugf("hello %v!", greeting)
 }
