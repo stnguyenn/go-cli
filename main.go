a3804b8f2528b7d53ad33f63dcd39c18e8b703a4
stnguyenn <42306483+stnguyenn@users.noreply.github.com>
2025-01-02 12:15:28 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+	"local/cmd"
 )
 
 func main() {
-	greeting := "world"
-	logrus.Debugf("hello %v!", greeting)
+	setConfig()
+	cmd.Execute()
+}
+
+func setConfig() {
+	viper.SetConfigName("config")         // name of config file (without extension)
+	viper.SetConfigType("yaml")           // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/etc/appname/")  // path to look for the config file in
+	viper.AddConfigPath("$HOME/.appname") // call multiple times to add many search paths
+	viper.AddConfigPath(".")              // optionally look for config in the working directory
+
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+
+	viper.WatchConfig()
 }
